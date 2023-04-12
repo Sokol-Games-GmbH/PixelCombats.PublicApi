a4af9f8d97e35b918466a67ceb965199f3fd6a5b
Konstantin <kkohno@mail.ru>
2023-04-13 05:08:16 +0700

    возможность определить ID блока, правка описания редактора

--- Api/RoomServer/Services/MapEditor/IMapEditorService.cs
+++ Api/RoomServer/Services/MapEditor/IMapEditorService.cs
@@ -11,24 +11,30 @@ namespace PixelCombats.Api.RoomServer.Services.MapEditor
 	public interface IMapEditorService
 	{
 		/// <summary>
-		/// задает воксель в указанной точке
+		/// задает блок в указанной точке
 		/// </summary>
 		/// <param name="x">координата x</param>
 		/// <param name="y">координата y</param>
 		/// <param name="z">координата z</param>
-		/// <param name="blockId">id блока</param>
+		/// <param name="blockId">id блока. 0 - стирает блоки</param>
 		void SetBlock(int x, int y, int z, ushort blockId);
+		/// <summary>
+		/// задает блок в указанной точке
+		/// </summary>
+		/// <param name="index">индекс точки</param>
+		/// <param name="blockId">id блока. 0 - стирает блоки</param>
+		void SetBlock(Index index, ushort blockId);
 		/// <summary>
 		/// закрашивает облать
 		/// </summary>
 		/// <param name="range">область</param>
-		/// <param name="blockId">id блока</param>
+		/// <param name="blockId">id блока. 0 - стирает блоки</param>
 		void SetBlock(IndexRange range, ushort blockId);
 		/// <summary>
 		/// закрашивает зону
 		/// </summary>
 		/// <param name="area">зона</param>
-		/// <param name="blockId">id блока</param>
+		/// <param name="blockId">id блока. 0 - стирает блоки</param>
 		void SetBlock(IArea area, ushort blockId);
 		/// <summary>
 		/// закрашивает область
@@ -39,7 +45,21 @@ namespace PixelCombats.Api.RoomServer.Services.MapEditor
 		/// <param name="xEnd">x конца (включительно)</param>
 		/// <param name="yEnd">y конца (включительно)</param>
 		/// <param name="zEnd">z конца (включительно)</param>
-		/// <param name="blockId"></param>
+		/// <param name="blockId">id блока. 0 - стирает блоки</param>
 		void SetBlockRange(int xStart, int yStart, int zStart, int xEnd, int yEnd, int zEnd, ushort blockId);
+		/// <summary>
+		/// возвращает ID блока в указанной точке
+		/// </summary>
+		/// <param name="x">координата x</param>
+		/// <param name="y">координата y</param>
+		/// <param name="z">координата z</param>
+		/// <returns>ID блока или 0, если в точке пусто</returns>
+		ushort GetBlockId(int x, int y, int z);
+		/// <summary>
+		/// возвращает ID блока в указанной точке
+		/// </summary>
+		/// <param name="index">индекс точки пространства</param>
+		/// <returns>ID блока или 0, если в точке пусто</returns>
+		ushort GetBlockId(Index index); 
 	}
 }
