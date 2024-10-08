8e47e9c30eb8000d13febad40d244c963f50b999
Konstantin <kkohno@mail.ru>
2024-10-08 20:58:49 +0700

    закрывание комнаты и файл разрешений режима

---
+++ Api/RoomServer/Services/Permissions/IPermissionsService.cs
@@ -0,0 +1,38 @@
+﻿using JavaScriptEngine.DataAnnotations;
+
+namespace PixelCombats.Api.RoomServer.Services.Permissions
+{
+	/// <summary>
+	/// структура разрешений в комнате
+	/// </summary>
+	public struct RoomPermissions
+	{
+		/// <summary>
+		/// можно ли закрывать комнату
+		/// </summary>
+		public bool CloseRoom { get; set; }
+
+		/// <summary>
+		/// разрешения в комнате по умолчанию
+		/// </summary>
+		public static RoomPermissions Default => new() {
+			CloseRoom = false
+		};
+	}
+	/// <summary>
+	/// 
+	/// </summary>
+	[ScriptObject("Permissions", ScriptModuleNames.ROOM_API)]
+	public interface IPermissionsService
+	{
+		/// <summary>
+		/// имя файла разрешений
+		/// </summary>
+		public const string GAME_MODE_ROOM_PERMISSIONS = "client/room/permissions.json";
+		/// <summary>
+		/// разрешения в комнате
+		/// <para>чтобы разрешения в комнате были нестандартные <see cref="RoomPermissions.Default"/> нужно при создании режима создать файл <see cref="GAME_MODE_ROOM_PERMISSIONS"/></para>
+		/// </summary>
+		RoomPermissions Value { get; }
+	}
+}
---
+++ Api/RoomServer/Services/Room/IRoomService.cs
@@ -0,0 +1,17 @@
+﻿using JavaScriptEngine.DataAnnotations;
+
+namespace PixelCombats.Api.RoomServer.Services.Room
+{
+	/// <summary>
+	/// сервис управления комнатой
+	/// </summary>
+	[ScriptObject("Room", ScriptModuleNames.ROOM_API)]
+	public interface IRoomService
+	{
+		/// <summary>
+		/// закрывает комнату
+		/// <para>заставляет всех, кто есть в комнате выйти из нее</para>
+		/// </summary>
+		void Close();
+	}
+}
